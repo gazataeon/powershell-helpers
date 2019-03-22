2db43a75a83fb1ed0f4dc41dc44696a1f6f32d0b
Gareth Lawson <gareth.lawson@mppglobal.com>
2019-03-22 14:12:31 +0800

    service principle update add

---
+++ azure/renew-service-principle-cert.ps1
@@ -0,0 +1,27 @@
+
+#Creates a self signed cert for 3 years
+
+$certName = Read-Host -Prompt "enter cert name"
+$spName = Read-Host -Prompt "enter cert name"
+
+$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" `
+  -Subject "CN=$($certName)" `
+  -KeySpec KeyExchange -NotAfter (get-date).AddYears(3)
+$keyValue = [System.Convert]::ToBase64String($cert.GetRawCertData())
+
+#export the cert as base64 to C:\temp\sp_cert.cer
+
+$cer = New-Object System.Security.Cryptography.X509Certificates.X509Certificate 
+$cer.Import("C:\temp\sp_cert.cer") 
+$binCert = $cer.GetRawCertData() 
+$credValue = [System.Convert]::ToBase64String($binCert)
+New-AzureRmADSpCredential -ServicePrincipalName "http://$($spName)" -CertValue $credValue
+
+
+#view current ones
+Get-AzureRmADSpCredential -ServicePrincipalName "http://$($spName)"
+
+
+
+# use this to remove existing ones
+#Remove-AzureRmADSpCredential -ServicePrincipalName http://jenkins_sp -KeyId 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
