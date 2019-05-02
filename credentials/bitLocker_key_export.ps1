4fced0ff6cd3dd1dc494584d2fda0e8ad18ddef6
Gareth Lawson <gareth.lawson@mppglobal.com>
2019-05-02 14:45:04 +0800

    oops!

--- credentials/bitLocker_key_export.ps1
+++ credentials/bitLocker_key_export.ps1
@@ -1,17 +1,19 @@
 #Script to export the recovery keys for the local machine and dump them in text files on the desktop for safe storage!
 
+$ErrorActionPreference = "Stop"
+
 #Change this if you like
-$targetLocation = "~\Desktop\bitlockerKeys"
+$targetLocation = "~/Desktop/bitlockerKeys"
 
 $encryptedVolumes = Get-BitLockerVolume | Where-Object "ProtectionStatus" -eq On
 
-if (!(Test-Path $targetLocation)){mkdir $encryptedVolumes}
+if (!(Test-Path $targetLocation)){mkdir $targetLocation}
 
 foreach($bitVol in $encryptedVolumes)
 {
     $driveLetter = $bitvol.MountPoint.chars(0)
     $keyinfo = (Get-BitLockerVolume -MountPoint $driveLetter).KeyProtector
-    $keyinfo | Out-File -FilePath "$encryptedVolumes\Drive_$($driveLetter).txt"
+    $keyinfo | Out-File -FilePath "$targetLocation\$($env:COMPUTERNAME)_Drive_$($driveLetter).txt"
 
 }
 
