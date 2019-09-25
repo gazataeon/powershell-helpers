886e21cae67830c544022cae18ea406aa1b93f85
Gareth Lawson <glawson@office.mppglobal.com>
2019-09-25 03:05:16 +0100

    revamp to the way the script works, it now works for sites without SNI

--- iis/update_iis_cert.ps1
+++ iis/update_iis_cert.ps1
@@ -1,19 +1,22 @@
-# This mad little script will make sure your new Cert in all the right places and update any IIS binding using the old one
+#################################################################################################################################################################
+### This mad little script will make sure your new Cert in all the right places and update any IIS binding using the old one 
+#**     You'll notice it only updates one cert initally, thats because any other bindings that dont use SNI will be updated automatically too. 
+#**     Additional runs should pick up any sites that use SNI. Feel free up adjust the last part with a loop if you like! 
+#################################################################################################################################################################
 
 Import-Module WebAdministration
+$ErrorActionPreference = "Stop"
 
-$oldhash = "EBxxxxxxxxxxxxxxxxxxxxxx721"
-$newhash = "EBxxxxxxxxxxxxxxxxxxxxxx722" # this needs updating lol
+## Variables ##
+$oldhash = "G16C16CE1539316CE1539316CE1539316CE15391" # find this with "Get-ChildItem -path cert:\LocalMachine\My"
+$newhash = "G16C16CE1539316CE1539316CE1539316CE15392" # 
 
-$pfxPass = "?????" # this too
-$pfxLocation = "c:\temp\certs\google.com.pfx" # check this ofc
+$pfxPass = "GeneralKenobi1234" # 
+$pfxLocation = "C:\Users\gazataeon\Downloads\gazataeon.com.pfx" # 
 
-$bindings = Get-WebBinding 
-
-$newcert = Get-ChildItem -Path Cert:\LocalMachine\My | where-Object {$_.Thumbprint -like $newhash} | Select-Object -ExpandProperty Thumbprint
-
-# Import Cert
-function Import-GazPfxCertificate {
+## Functions ##
+function Import-GazPfxCertificate 
+{
     param 
     (
         [String]$certPath,
@@ -29,33 +32,56 @@ function Import-GazPfxCertificate {
     $store.close()
 }
 
+## Body ##
+Write-host "Importing the PFX to the cert stores." -ForegroundColor Yellow
 Import-GazPfxCertificate -certPath $pfxLocation
 Import-GazPfxCertificate -certPath $pfxLocation -certRootStore "CurrentUser" -certStore "My"
 Import-GazPfxCertificate -certPath $pfxLocation -certStore "My"
+$newcert = Get-ChildItem -Path Cert:\LocalMachine\My | where-Object {$_.Thumbprint -like $newhash} | Select-Object -ExpandProperty Thumbprint
+Write-host "Cert Imported! Let's update IIS now!" -ForegroundColor Green
 
-
-#Replace cert
-foreach ($binding in $bindings)
-{
-if ($binding.certificateHash -ne $null){$siteCertHashStr = $binding.certificatehash.ToString()}
-write-host "checking site :$($binding.bindingInformation.ToString())"
-    if ($binding.certificatehash -eq $oldhash)
-    {
-        $sitename = $binding.ItemXPath
-        $sitename = $sitename -replace ".\w*.\w*.\w*.\w*.(@name=')"
-        $sitename = $sitename -replace "'\sand\s@id='\d']"
-        $fqdn = $binding.bindingInformation.split(':').item(2)
-        $ipaddress = $binding.bindingInformation.split(':').item(0)
-      
-        write-host "Updating Binding: $sitename" -ForegroundColor Green
-	    $binding | remove-webbinding
-        New-WebBinding -Name $sitename -Port 443 -Protocol https -HostHeader $fqdn -IPAddress $ipaddress
-	(Get-WebBinding -Name $sitename -Port 443 -Protocol "https" -HostHeader $fqdn).AddSslCertificate($newcert, "my") 
-    }
-    else
-    {
-        write-host "skipping $($binding.bindingInformation.ToString())" -ForegroundColor yellow
-    }
+write-host "Checking for IIS sites that use the old Cert Hash" -ForegroundColor Yellow
+$bindings = Get-WebBinding 
+[array]$hashlist = $null
+foreach($certHashItem in ($bindings | Select-Object certificateHash)){
+$hashList += $certHashItem.certificateHash
 }
 
+# Check if there is an old hash in use?
+if ($hashList.Contains($oldhash))
+{
+    $oldBindings = $bindings | Where-Object certificateHash -eq $oldhash
+    write-host "Found $($oldBindings.count) bindings that use the old hash!" -ForegroundColor Yellow
+    #Update one binding first
+    $bindingToUpdate = $oldBindings | Select-Object * -First 1
 
+    $sitename = $bindingToUpdate.ItemXPath
+    $sitename = $sitename -replace ".\w*.\w*.\w*.\w*.(@name=')"
+    $sitename = $sitename -replace "'\sand\s@id='\d']"
+    $fqdn = $bindingToUpdate.bindingInformation.split(':').item(2)
+    $ipaddress = $bindingToUpdate.bindingInformation.split(':').item(0)
+    write-host "Updating $($sitename) first " -ForegroundColor Yellow
+
+    write-host "Removing Old Binding: $sitename" -ForegroundColor Yellow
+    $bindingToUpdate | remove-webbinding
+
+    write-host "adding new Binding: $sitename" -ForegroundColor Yellow
+    New-WebBinding -Name $sitename -Port 443 -Protocol https -HostHeader $fqdn -IPAddress $ipaddress
+    (Get-WebBinding -Name $sitename -Port 443 -Protocol "https" -HostHeader $fqdn).AddSslCertificate($newcert, "my") 
+}
+write-host "Re checking bindings to make sure they all updated" -ForegroundColor Yellow
+$bindings = Get-WebBinding 
+[array]$hashlist = $null
+foreach($certHashItem in ($bindings | Select-Object certificateHash))
+{
+$hashList += $certHashItem.certificateHash
+}
+
+# is there an old hash in use?
+if ($hashList.Contains($oldhash))
+{
+Write-host "Looks like there's a site still using the old hash!, rerun this and it should clear up. If not, seek a Doctor." -ForegroundColor red
+$bindings | Where-Object certificateHash -Like $oldhash
+}
+else
+{ write-host "No old hashes found!" -ForegroundColor Green }
